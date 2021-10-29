f413c998741b36636f0a89a211cda1d109ff754c
ZhuoranTian <849597043@qq.com>
2021-10-29 18:18:57 +0800

    Test

---
+++ test.cpp
@@ -0,0 +1,20 @@
+//test
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "hello world, hello C++" << endl;
+
+	// system("pause");
+	int a = 1;
+	int b = 2;
+	if (a && b){
+		cout << "true" << endl;
+	}
+	else
+	{
+		cout << "false" << endl;
+	}
+	return 0;
+}
