9d9386d1dcddd9763984c83d31d18de2ad732596
ZhuoranTian <849597043@qq.com>
2021-11-02 21:28:55 +0800

    test

---
+++ class.cpp
@@ -0,0 +1,18 @@
+//test
+#include <iostream>
+using namespace std;
+
+class A1
+{
+public:
+    A1(); //声明构造函数
+};
+A1::A1() //定义构造函数
+{
+    cout << 123 << endl;
+}
+int main(void)
+{
+
+    return 0;
+}
